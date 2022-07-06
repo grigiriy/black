fc50b6b11675da1579a84db5afabf9d342e61c9b
Grigiriy <grigiriy.malyshev@gmail.com>
2022-07-07 00:57:04 +0300

    form lay

---
+++ black/template-parts/section-form.php
@@ -0,0 +1,33 @@
+<section class="form">
+  <div class="wrapper">
+    <p class="headline">BOOK AN EXOTIC TODAY</p>
+    <p>Give us your information and we will contact you in few minutes.</p>
+    <div class="form-wrapper">
+      <form action="">
+        <div>
+          <label for=""><span>*</span> First Name</label>
+          <input type="text" required>
+        </div>
+        <div>
+          <label for="">Last Name</label>
+          <input type="text">
+        </div>
+        <div class="full">
+          <label for=""><span>*</span> Email</label>
+          <input type="text" required>
+        </div>
+        <div>
+          <label for=""><span>*</span> Phone</label>
+          <input type="text" required>
+        </div>
+        <div>
+          <label for=""><span>*</span> Selected vehicle</label>
+          <input type="text" required>
+        </div>
+          <div class="full">
+          <input type="submit" value="Submit">
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
