71d5fd43edc8ad0cb63a5fca0b83e804a28e772f
Grigiriy <grigiriy.malyshev@gmail.com>
2022-07-07 16:52:49 +0300

    burger

--- black/template-parts/burger.php
+++ black/template-parts/burger.php
@@ -1,51 +1,26 @@
-<div id="burger" class="drawer drawer--left">
-  <div class="drawer__inner">
-    <form action="/search" method="get" class="drawer__search" role="search">
-      <input type="search" name="q" placeholder="Search" aria-label="Search" class="drawer__search-input">
-
-      <button type="submit" class="text-link drawer__search-submit">
-        <svg aria-hidden="true" focusable="false" role="presentation" class="icon icon-search" viewBox="0 0 32 32">
-          <path fill="#444" d="M21.839 18.771a10.012 10.012 0 0 0 1.57-5.39c0-5.548-4.493-10.048-10.034-10.048-5.548 0-10.041 4.499-10.041 10.048s4.493 10.048 10.034 10.048c2.012 0 3.886-.594 5.456-1.61l.455-.317 7.165 7.165 2.223-2.263-7.158-7.165.33-.468zM18.995 7.767c1.498 1.498 2.322 3.49 2.322 5.608s-.825 4.11-2.322 5.608c-1.498 1.498-3.49 2.322-5.608 2.322s-4.11-.825-5.608-2.322c-1.498-1.498-2.322-3.49-2.322-5.608s.825-4.11 2.322-5.608c1.498-1.498 3.49-2.322 5.608-2.322s4.11.825 5.608 2.322z"></path>
+<div id="burger">
+  <div class="bar">
+    <form action="/" method="get" role="search">
+      <input type="search" name="q" placeholder="Search" aria-label="Search">
+      <button type="submit">
+        <svg aria-hidden="true" focusable="false" role="presentation" viewBox="0 0 32 32">
+          <path d="M21.839 18.771a10.012 10.012 0 0 0 1.57-5.39c0-5.548-4.493-10.048-10.034-10.048-5.548 0-10.041 4.499-10.041 10.048s4.493 10.048 10.034 10.048c2.012 0 3.886-.594 5.456-1.61l.455-.317 7.165 7.165 2.223-2.263-7.158-7.165.33-.468zM18.995 7.767c1.498 1.498 2.322 3.49 2.322 5.608s-.825 4.11-2.322 5.608c-1.498 1.498-3.49 2.322-5.608 2.322s-4.11-.825-5.608-2.322c-1.498-1.498-2.322-3.49-2.322-5.608s.825-4.11 2.322-5.608c1.498-1.498 3.49-2.322 5.608-2.322s4.11.825 5.608 2.322z"></path>
         </svg>
-        <span class="icon__fallback-text">Search</span>
       </button>
     </form>
-    <ul class="drawer__nav">
-      <li class="drawer__nav-item">
-        <a href="/" class="drawer__nav-link drawer__nav-link--top-level" aria-current="page">
-          HOME
-        </a>
-      </li>
-      <li class="drawer__nav-item">
-        <a href="/pages/fleet" class="drawer__nav-link drawer__nav-link--top-level">
-          OUR FLEET
-        </a>
-      </li>
-      <li class="drawer__nav-item">
-        <a href="/pages/services" class="drawer__nav-link drawer__nav-link--top-level">
-          SERVICES
-        </a>
-      </li>
-      <li class="drawer__nav-item">
-        <a href="/pages/about-us" class="drawer__nav-link drawer__nav-link--top-level">
-          ABOUT
-        </a>
-      </li>
-      <li class="drawer__nav-item">
-        <a href="/pages/contact-us-1" class="drawer__nav-link drawer__nav-link--top-level">
-          CONTACT US
-        </a>
-      </li>
-      <li class="drawer__nav-item">
-        <a href="/pages/refund-policy" class="drawer__nav-link drawer__nav-link--top-level">
-          REFUND POLICY
-        </a>
-      </li>
-      <li class="drawer__nav-item">
-        <a href="/pages/reserve" class="drawer__nav-link drawer__nav-link--top-level">
-          RESERVE
-        </a>
-      </li>
+    <ul class="nav">
+      <li class="active"><a href="/">Home</a></li>
+      <li><a href="/#catalog">Our fleet</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="/#about">About</a></li>
+      <li><a href="#">Contact us</a></li>
+      <li><a href="#">Refund Policy</a></li>
+      <li><a href="/#book">Reserve</a></li>
     </ul>
+  </div>
+  <div class="cross">
+    <svg aria-hidden="true" focusable="false" role="presentation" viewBox="0 0 32 32">
+      <path fill="#444" d="M25.313 8.55l-1.862-1.862-7.45 7.45-7.45-7.45L6.689 8.55l7.45 7.45-7.45 7.45 1.862 1.862 7.45-7.45 7.45 7.45 1.862-1.862-7.45-7.45z"></path>
+    </svg>
   </div>
 </div>
