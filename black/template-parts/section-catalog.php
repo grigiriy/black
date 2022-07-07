03cba35d402141724c304a48dfd4f23a70e245bb
Grigiriy <grigiriy.malyshev@gmail.com>
2022-07-07 10:06:52 +0300

    catalog and lil refactoring

--- black/template-parts/section-catalog.php
+++ black/template-parts/section-catalog.php
@@ -1,3 +1,22 @@
 <section class="catalog">
   <p class="headline">Our Fleet</p>
+  <ul class="cats-list">
+    <li class="selected">All cars</li>
+    <li>Lamborghini</li>
+    <li>Rolls Royce</li>
+    <li>Bentley</li>
+    <li>Ferrari</li>
+    <li>Mercedes</li>
+    <li>Slingshot</li>
+  </ul>
+  <ul class="cars-list">
+    <?php for ($i=0;$i<12;$i++){ ?>
+    <li>
+      <a href="#">
+        <img src="https://cdn.shopify.com/s/files/1/0587/6875/2824/products/ScreenShot2022-04-19at7.39.14PM.png" alt="2020 McLaren 570S">
+      </a>
+      <a href="#">2020 McLaren 570S</a>
+    </li>
+    <?php } ?>
+  </ul>
 </section>
